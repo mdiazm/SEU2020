2bdf96ea81ef6b03118d024643889e263b940baa
Gema Parra Cabrera <gpc00014@red.ujaen.es>
2020-02-28 20:13:05 +0100

    modificaciones en la configuración para aware

--- Client/app/src/main/java/com/seu/sensors/MainActivity.java
+++ Client/app/src/main/java/com/seu/sensors/MainActivity.java
@@ -1,13 +1,18 @@
 package com.seu.sensors;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.aware.Accelerometer;
+import com.aware.Applications;
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
 
@@ -23,11 +28,18 @@ public class MainActivity extends AppCompatActivity {
 
         Context context = getApplicationContext();
 
-        // Aware framework
-        Aware.startAWARE(context);
-        Aware.setSetting(context, Aware_Preferences.FREQUENCY_ACCELEROMETER, 200000);
-        Aware.setSetting(context, Aware_Preferences.THRESHOLD_ACCELEROMETER, 0.02f);
+        Intent aware = new Intent(this, Aware.class);
+        startService(aware);
+        Aware.startAWARE(this);
+        Aware.setSetting(this, Aware_Preferences.STATUS_APPLICATIONS, true);
+        Applications.isAccessibilityServiceActive(getApplicationContext());
 
-        Aware.startAccelerometer(this);
+        // Aware framework
+         //   Aware.startAWARE(context);
+            Aware.setSetting(context, Aware_Preferences.FREQUENCY_ACCELEROMETER, 200000);
+            Aware.setSetting(context, Aware_Preferences.THRESHOLD_ACCELEROMETER, 0.02f);
+
+            Aware.startAccelerometer(this);
+
     }
 }
