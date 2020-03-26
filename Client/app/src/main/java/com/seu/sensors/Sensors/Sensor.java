1978484d0208de6601f91181de3cefb579d67036
Gema Parra Cabrera <gpc00014@red.ujaen.es>
2020-03-26 10:53:28 +0100

    comentarios

--- Client/app/src/main/java/com/seu/sensors/Sensors/Sensor.java
+++ Client/app/src/main/java/com/seu/sensors/Sensors/Sensor.java
@@ -2,15 +2,27 @@ package com.seu.sensors.Sensors;
 
 import android.content.Context;
 import com.aware.Aware;
+
+/**
+ * Clase que define un sensor por defecto
+ * */
 public class Sensor {
 
-    private String name;
-    private boolean state;
-    private int image;
-    private String key;
+    private String name; ///> Nombre del sensor
+    private boolean state; ///> Estado del sensor
+    private int image; ///> Imagen del sensor
+    private String key; ///> Clave del sensor
 
-    Context context;
+    Context context; ///> Contexto
 
+    /**
+     * Constructor parametrizado
+     * @param c
+     * @param image
+     * @param key
+     * @param name
+     * @param state
+     * */
     public Sensor(String name, boolean state, int image, String key, Context c){
         this.name = name;
         this.state = state;
@@ -19,14 +31,25 @@ public class Sensor {
         this.key = key;
     }
 
+    /**
+     * Método para obteneer el nombre del sensor
+     * */
     public String getName() {
         return name;
     }
 
+    /**
+     * Método para establecer el nombre del sensor
+     * @param name nombre del sensor
+     * */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Método para establecer el estado del sensor
+     * @param state estado del sensor
+     * */
     public void setState(boolean state) {
         if(state){
             if(name == "Acelerómetro") {
@@ -74,30 +97,52 @@ public class Sensor {
         this.state = state;
     }
 
+    /**
+     * Método para obtener el estado del sensor
+     * */
     public boolean getState() {
         return state;
     }
 
+    /**
+     * Método para establecer la imagen del sensor
+     * @param image imagen para el sensor
+     * */
     public void setImage(int image) {
         this.image = image;
     }
 
+    /**
+     * Método para obtener la imagen del sensor
+     * */
     public int getImage() {
         return image;
     }
 
+    /**
+     * Método para obtener la clave del sensor
+     * */
     public String getKey() {
         return key;
     }
 
+    /**
+     * Método para establecer la clave del sensor
+     * */
     public void setKey(String key) {
         this.key = key;
     }
 
+    /**
+     * Método para obtener el contexto
+     * */
     public Context getContext() {
         return context;
     }
 
+    /**
+     * Método para establecer el contexto
+     * */
     public void setContext(Context context) {
         this.context = context;
     }
